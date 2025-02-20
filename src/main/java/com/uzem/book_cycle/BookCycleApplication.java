ece9f09fcbf0914827e0fcb5cfe097491887ba85
yujin <100543186+uzemm@users.noreply.github.com>
2025-02-21 00:09:35 +0900

    Initial commit

---
+++ src/main/java/com/uzem/book_cycle/BookCycleApplication.java
@@ -0,0 +1,13 @@
+package com.uzem.book_cycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCycleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uzem/book_cycle/BookCycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uzem.book_cycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
