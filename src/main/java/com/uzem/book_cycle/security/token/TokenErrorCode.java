5f02c05c6800e05faaa385ba3680198d0656ba68
yujin <100543186+uzemm@users.noreply.github.com>
2025-03-01 17:11:08 +0900

    fix: JWT 예외 코드 수정

--- src/main/java/com/uzem/book_cycle/security/token/TokenErrorCode.java
+++ src/main/java/com/uzem/book_cycle/security/token/TokenErrorCode.java
@@ -6,12 +6,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TokenErrorCode {
-    INVALID_REFRESH_TOKEN("유효하지 않은 리프레시 토큰입니다."),
     NOT_A_REFRESH_TOKEN("리프레시 토큰이 아닙니다."),
-    REFRESH_TOKEN_EXPIRED("리프레시 토큰이 만료되었습니다."),
     TOKEN_ALREADY_LOGGED_OUT("로그아웃된 토큰입니다."),
+    INVALID_TOKEN("잘못된 JWT 서명입니다."),
+    EXPIRED_TOKEN("만료된 JWT 토큰입니다."),
     UNSUPPORTED_TOKEN("지원되지 않는 JWT 토큰입니다."),
-    ILLEGAL_ARGUMENT("JWT 토큰이 잘못되었습니다.")
+    ILLEGAL_TOKEN("JWT 토큰이 없습니다.")
     ;
 
     private String description;
