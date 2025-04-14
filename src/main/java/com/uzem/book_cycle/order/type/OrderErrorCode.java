aa80591584d29b3936e55c3628e343c0d0c0b47c
yujin <100543186+uzemm@users.noreply.github.com>
2025-04-14 17:03:39 +0900

    feat: GlobalExceptionHandler에 관심도서, 장바구니, 주문 예외 처리 추가

--- src/main/java/com/uzem/book_cycle/order/type/OrderErrorCode.java
+++ src/main/java/com/uzem/book_cycle/order/type/OrderErrorCode.java
@@ -1,11 +1,12 @@
 package com.uzem.book_cycle.order.type;
 
+import com.uzem.book_cycle.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum OrderErrorCode {
+public enum OrderErrorCode implements ErrorCode {
     ORDER_NOT_FOUND("주문내역을 찾을 수 없습니다."),
     INVALID_TOTAL_PRICE("총 결제 금액은 0원보다 커야 합니다."),
     ORDER_ITEM_NOT_FOUND("주문 항목이 없습니다."),
@@ -13,4 +14,14 @@ public enum OrderErrorCode {
     ;
 
     private String description;
+
+    @Override
+    public String getCode() {
+        return this.name();
+    }
+
+    @Override
+    public String getMessage() {
+        return this.description;
+    }
 }
