c3de99662a1b6cb027781dc114c0c70f7f822861
yujin <100543186+uzemm@users.noreply.github.com>
2025-04-15 22:35:55 +0900

    feat: SwaggerConfig에 JWT 인증 설정 추가
    - 보안 스키마에 Authorization 헤더 설정 (bearer + JWT)
    - 전역 SecurityRequirement 적용

--- src/main/java/com/uzem/book_cycle/config/SwaggerConfig.java
+++ src/main/java/com/uzem/book_cycle/config/SwaggerConfig.java
@@ -3,20 +3,36 @@ package com.uzem.book_cycle.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+        // Security Scheme 정의
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        // Security Requirement 정의
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components().addSecuritySchemes("Authorization", securityScheme))
+                .addSecurityItem(securityRequirement)
                 .info(apiInfo());
     }
+
     private Info apiInfo() {
         return new Info()
-                .title("Springdoc 테스트")
-                .description("Springdoc을 사용한 Swagger UI 테스트");
+                .title("BookCycle API 문서")
+                .description("중고 도서 거래 및 대여 서비스 API 명세서")
+                .version("v1.0.0");
     }
 }
