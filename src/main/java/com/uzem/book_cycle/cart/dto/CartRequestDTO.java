f430b2e3a68a838c18a9fb63724b8115ab4ef3a6
yujin <100543186+uzemm@users.noreply.github.com>
2025-05-21 18:27:38 +0900

    docs: Swagger 문서용 어노테이션 추가
    - Swagger 문서 그룹핑 및 설명 개선

--- src/main/java/com/uzem/book_cycle/cart/dto/CartRequestDTO.java
+++ src/main/java/com/uzem/book_cycle/cart/dto/CartRequestDTO.java
@@ -1,6 +1,7 @@
 package com.uzem.book_cycle.cart.dto;
 
 import com.uzem.book_cycle.order.type.ItemType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,10 +12,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "장바구니 요청 DTO")
 public class CartRequestDTO {
 
     @NotNull
+    @Schema(description = "도서id", example = "1")
     private Long bookId;
+
+    @Schema(description = "도서타입", example = "SALE/RENTAL")
     @NotNull
     private ItemType itemType;
 
