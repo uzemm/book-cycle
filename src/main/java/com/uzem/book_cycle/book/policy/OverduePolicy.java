9ecfc8c1f4ec087293c319d72499624e7fe8f91f
yujin <100543186+uzemm@users.noreply.github.com>
2025-07-15 00:40:32 +0900

    feat: 연체료 계산 로직 정책 기반으로 수정
    - 기존: 연체일수 × 도서 가격
    - 변경: 7일 이하 0원, 이후 1일당 500원 부과

--- src/main/java/com/uzem/book_cycle/book/policy/OverduePolicy.java
+++ src/main/java/com/uzem/book_cycle/book/policy/OverduePolicy.java
@@ -1,12 +1,19 @@
 package com.uzem.book_cycle.book.policy;
 
-import com.uzem.book_cycle.book.entity.RentalHistory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OverduePolicy {
 
-    public long calculateOverdue(RentalHistory rentalHistory, long overdueDays) {
-        return rentalHistory.getPrice() * overdueDays;
+    public long calculateOverdue(long overdueDays) {
+
+        if(overdueDays <= 7) return 0;
+
+        if(overdueDays <= 30) {
+            return (overdueDays - 7) * 500;
+        }
+
+        // 23일치만 부과 (8~30일 = 23일간)
+        return 500 * 23;
     }
 }
