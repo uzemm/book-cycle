5a3d004ec845a79b958fd058220aa6df9d73c864
yujin <100543186+uzemm@users.noreply.github.com>
2025-07-23 02:16:39 +0900

    refactor: Event 클래스를 record로 전환
    - RentalOverdueEvent, OverdueFeeEvent를 record 헤더로 이동
    - 불필요해진 생성자 및 getter 제거

--- src/main/java/com/uzem/book_cycle/event/OverdueFeeEvent.java
+++ src/main/java/com/uzem/book_cycle/event/OverdueFeeEvent.java
@@ -2,17 +2,8 @@ package com.uzem.book_cycle.event;
 
 
 import com.uzem.book_cycle.book.entity.RentalHistory;
-import lombok.Getter;
 
 
-@Getter
-public class OverdueFeeEvent{
+public record OverdueFeeEvent(RentalHistory rentalHistory, long overdueDays) {
 
-    private final RentalHistory rentalHistory;
-    private final long overdueDays;
-
-    public OverdueFeeEvent(RentalHistory rentalHistory, long overdueDays) {
-        this.rentalHistory = rentalHistory;
-        this.overdueDays = overdueDays;
-    }
 }
